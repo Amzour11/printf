52d67d36356155a9fa4a1ee1a383dbeb1dff5f65
Mohamed Amzour <mohamedamzour11@gmail.com>
2023-04-28 18:32:27 -0700

    printf project commits

--- main.h
+++ main.h
@@ -64,11 +64,11 @@ typedef struct specifier
 	int (*f)(va_list, params_t *);
 } specifier_t;
 
-/* _put.c */
+/* _put.c module */
 int _puts(char *str);
 int _putchar(int c);
 
-/* print_functions.c */
+/* print_functions.c module */
 int print_char(va_list ap, params_t *params);
 int print_int(va_list ap, params_t *params);
 int print_string(va_list ap, params_t *params);
@@ -80,38 +80,38 @@ char *convert(long int num, int base, int flags, params_t *params);
 int print_unsigned(va_list ap, params_t *params);
 int print_address(va_list ap, params_t *params);
 
-/* specifier.c */
+/* specifier.c module */
 int (*get_specifier(char *s))(va_list ap, params_t *params);
 int get_print_func(char *s, va_list ap, params_t *params);
 int get_flag(char *s, params_t *params);
 int get_modifier(char *s, params_t *params);
 char *get_width(char *s, params_t *params, va_list ap);
 
-/* convert_number.c */
+/* convert_number.c module */
 int print_hex(va_list ap, params_t *params);
 int print_HEX(va_list ap, params_t *params);
 int print_binary(va_list ap, params_t *params);
 int print_octal(va_list ap, params_t *params);
 
-/* sipmle_printers.c */
+/* sipmle_printers.c module */
 int print_from_to(char *start, char *stop, char *except);
 int print_rev(va_list ap, params_t *params);
 int print_rot13(va_list ap, params_t *params);
 
-/* print_number.c */
+/* print_number.c module */
 int _isdigit(int c);
 int _strlen(char *s);
 int print_number(char *str, params_t *params);
 int print_number_right_shift(char *str, params_t *params);
 int print_number_left_shift(char *str, params_t *params);
 
-/* params.c */
+/* params.c module */
 void init_params(params_t *params, va_list ap);
 
-/* string_fields.c */
+/* string_fields.c module */
 char *get_precision(char *p, params_t *params, va_list ap);
 
-/* _printf.c */
+/* _printf.c module */
 int _printf(const char *format, ...);
 
 #endif
